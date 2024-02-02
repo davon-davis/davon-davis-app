e4b25596f1e83e0b78037bdd29fb9d7ae3d4e1a8
Davon Davis <davond009@gmail.com>
2024-02-02 14:25:26 -0500

    modified co yaml

--- dist/server.js
+++ dist/server.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3003;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    res.json({ message: "Hello Davon L Davis!" });
+app.use(express_1.default.static(path_1.default.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '/frontend/dist', 'index.html'));
 });
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
