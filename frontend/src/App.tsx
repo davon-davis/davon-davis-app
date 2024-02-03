543ba32519aa75abe97efa9dffd66447be14baed
Davon Davis <davond009@gmail.com>
2024-02-03 17:49:15 -0500

    change homepage to Davon Davis

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,9 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>WE ARE LIVE BABY!!</h1>
-        </>
+        <h1>Davon Davis</h1>
     )
 }
 
