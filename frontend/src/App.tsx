40f29c2074fed2c6c28f097875b903fa143e0f35
Davon Davis <davond009@gmail.com>
2024-02-03 11:43:46 -0500

    change homepage header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
                     <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>WE ARE LIVE BABY!!</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
