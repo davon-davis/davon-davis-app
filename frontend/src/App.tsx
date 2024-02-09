b12604fbbb23e9d15714c67ec38dbb4a06c09fff
Davon Davis <davond009@gmail.com>
2024-02-08 22:30:59 -0500

    add shadcn badge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import "./index.css";
+import { Badge } from "@/components/ui/badge.tsx";
 
 function App() {
   return (
-    <body className="flex justify-center">
-      <h1 className=" text-xl font-bold">
-        Hello, I'm <span className="text-yellow-400">Davon Davis</span>
+    <body className="flex flex-col justify-center">
+      <h1 className=" text-xl font-bold mb-2">
+        Hello! I'm <span className="text-yellow-400">Davon Davis</span>
       </h1>
+      <p>
+        A <span className={"font-bold"}>Full Stack Developer</span>
+      </p>
+      <p>
+        with
+        <Badge className={"mx-1 text-yellow-400"}>3.2 Years</Badge>
+        Experience
+      </p>
     </body>
   );
 }
